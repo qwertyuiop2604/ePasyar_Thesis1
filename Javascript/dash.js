2e083447cf894e05d0299d89f0fc4de7fed3b379
qwertyuiop2604 <uiop05541@gmail.com>
2024-05-17 13:57:36 -0400

    commit

--- Javascript/dash.js
+++ Javascript/dash.js
@@ -98,20 +98,34 @@ reviews.addEventListener("click", () => {
 // Sample data for most visited locations
 const mostVisitedData = {
   labels: [
-    "Location 1",
-    "Location 2",
-    "Location 3",
-    "Location 4",
-    "Location 5",
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ],
   datasets: [
     {
       label: "Number of Visitors",
-      data: [100, 200, 150, 250, 180], // Replace with your actual data
+      data: [100, 200, 150, 250, 180, 100, 200, 150, 250, 180, 100, 130], // Replace with your actual data
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
         "rgba(54, 162, 235, 0.2)",
         "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
         "rgba(75, 192, 192, 0.2)",
         "rgba(153, 102, 255, 0.2)",
       ],
@@ -121,10 +135,18 @@ const mostVisitedData = {
         "rgba(255, 206, 86, 1)",
         "rgba(75, 192, 192, 1)",
         "rgba(153, 102, 255, 1)",
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
       ],
       borderWidth: 1,
     },
   ],
+  
 };
 
 // Get the canvas element
@@ -132,7 +154,7 @@ const ctx = document.getElementById("mostVisitedChart").getContext("2d");
 
 // Create the bar chart
 const mostVisitedChart = new Chart(ctx, {
-  type: "bar",
+  type: "line",
   data: mostVisitedData,
   options: {
     scales: {
@@ -142,6 +164,8 @@ const mostVisitedChart = new Chart(ctx, {
     },
   },
 });
+
+
 async function fetchTourists() {
   const touristsTable = document.getElementById("touristsTableBody");
 
